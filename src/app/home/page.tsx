d859acfb36ea073312bc81e3913a9b7432f2797b
Namsraijav-Davaa <namsraijav0415@gmail.com>
2023-08-18 14:57:59 +0800

    namsrai push

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,10 +13,13 @@ import { Page4 } from "./page4";
 import { Page5 } from "./page5";
 import { Page1 } from "./page1";
 import { Page2 } from "./page2";
+import { Box, Grid, useMediaQuery, useTheme } from "@mui/material";
+import MobileResponsive from "./mobileScreen";
 
 export default function HomePage() {
     const { setColor } = useContext(ColorContext);
-
+    const theme = useTheme();
+    const isMdScreen = useMediaQuery(theme.breakpoints.up("md"));
     const onSlideChange = (e: any) => {
         if (e.activeIndex == 2 || e.activeIndex == 3) {
             setColor("black");
@@ -25,39 +28,41 @@ export default function HomePage() {
         }
     };
 
+    if (!isMdScreen) {
+        return <MobileResponsive />;
+    }
+
     return (
-        <>
-            <Swiper
-                direction={"vertical"}
-                slidesPerView={1}
-                mousewheel={true}
-                speed={1300}
-                pagination={{
-                    clickable: true,
-                }}
-                scrollbar={{ draggable: true }}
-                modules={[Mousewheel, Pagination]}
-                className="mySwiper"
-                onSlideChange={onSlideChange}
-                touchEventsTarget="container"
-                touchReleaseOnEdges={true}
-            >
-                <SwiperSlide>
-                    {({ isActive }) => <Page1 isActive={isActive} />}
-                </SwiperSlide>
-                <SwiperSlide>
-                    {({ isActive }) => <Page2 isActive={isActive} />}
-                </SwiperSlide>
-                <SwiperSlide>
-                    {({ isActive }) => <Page3 isActive={isActive} />}
-                </SwiperSlide>
-                <SwiperSlide>
-                    {({ isActive }) => <Page4 isActive={isActive} />}
-                </SwiperSlide>
-                <SwiperSlide>
-                    {({ isActive }) => <Page5 isActive={isActive} />}
-                </SwiperSlide>
-            </Swiper>
-        </>
+        <Swiper
+            direction={"vertical"}
+            slidesPerView={1}
+            mousewheel={true}
+            speed={1300}
+            pagination={{
+                clickable: true,
+            }}
+            scrollbar={{ draggable: true }}
+            modules={[Mousewheel, Pagination]}
+            className="mySwiper"
+            onSlideChange={onSlideChange}
+            touchEventsTarget="container"
+            touchReleaseOnEdges={true}
+        >
+            <SwiperSlide>
+                {({ isActive }) => <Page1 isActive={isActive} />}
+            </SwiperSlide>
+            <SwiperSlide>
+                {({ isActive }) => <Page2 isActive={isActive} />}
+            </SwiperSlide>
+            <SwiperSlide>
+                {({ isActive }) => <Page3 isActive={isActive} />}
+            </SwiperSlide>
+            <SwiperSlide>
+                {({ isActive }) => <Page4 isActive={isActive} />}
+            </SwiperSlide>
+            <SwiperSlide>
+                {({ isActive }) => <Page5 isActive={isActive} />}
+            </SwiperSlide>
+        </Swiper>
     );
 }
